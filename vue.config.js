0839ddb4f5ae4957f319ab1f8e13525d9ea4df17
Ythotha <yaroslav.pon@gmail.com>
2023-09-04 20:01:52 +0300

    changed publicPath for GH Pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/Vue-3-inventory/',
   transpileDependencies: true,
   // css: {
   //   loaderOptions: {
